898829c0a8ce39a08681fb117c96bd8290e5ce5c
chilung <chilung.cs06g@nctu.edu.tw>
2021-03-19 08:46:19 +0000

    using /home/jovyan directory

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-PROJ_ROOT="/home/ccma/MDD"
+PROJ_ROOT=$HOME'/MDD'
 PROJ_NAME="MDD"
 LOG_FILE="${PROJ_ROOT}/log/${PROJ_NAME}-`date +'%Y-%m-%d-%H-%M-%S'`.log"
 
@@ -10,6 +10,6 @@ echo "GPU: $CUDA_VISIBLE_DEVICES" > ${LOG_FILE}
 python ${PROJ_ROOT}/trainer/train.py \
     --config ${PROJ_ROOT}/config/dann.yml \
     --dataset Office-31 \
-    --src_address /home/ccma/MDD/data/dslr.txt \
-    --tgt_address /home/ccma/MDD/data/amazon.txt \
+    --src_address $HOME/MDD/data/dslr.txt \
+    --tgt_address $HOME/MDD/data/amazon.txt \
     >> ${LOG_FILE}  2>&1
